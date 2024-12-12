ac780c0aeb8b4d7ccb5d354046746e758de8657a
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-12 16:03:18 -0300

    feat: add desafio loan

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,14 @@
-import { DioAccount } from "./DioAccount"
+import { DioAccount } from "./DioAccount";
+
+// [X] Implementar o método de empréstimo (getLoan) na classe CompanyAccount
+// Os valores do saldos deve ser acrescidos, de acordo com o valor informado para empréstimo
+// Apenas contas com o status true podem fazer empréstimo
 
 export class CompanyAccount extends DioAccount {
-
-  constructor(name: string, accountNumber: number){
-    super(name, accountNumber)
+  constructor(name: string, accountNumber: number) {
+    super(name, accountNumber);
   }
-
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
-  }
+  getLoan = (valueLoan: number) => {
+    this.deposit(valueLoan);
+  };
 }
