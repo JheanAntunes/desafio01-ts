570eb506138d7e0f8d4e0f5b8204751f74ca1b9f
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-12 16:02:32 -0300

    feat: add call class

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { DioAccountVip } from "./class/DioAccountVip";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount = new PeopleAccount(1, "jhean", 10);
+console.log(peopleAccount.getName());
+console.log(peopleAccount.doc_id);
+
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20);
+companyAccount.deposit(100);
+console.log("deposit: ", companyAccount.getBalance()); // output balance 100
+companyAccount.withdraw(80);
+console.log("withdraw: ", companyAccount.getBalance()); // output balance 20
+companyAccount.getLoan(180);
+console.log("emprestimo: ", companyAccount.getBalance()); // output balance 200
+
+const peopleAccountVip = new DioAccountVip("jheanVip", 51);
+peopleAccountVip.deposit(100); // output 110
